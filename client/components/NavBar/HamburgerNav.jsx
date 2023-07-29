416ebc8587e6a01e35dec3a8e00cc5dbc6e9a75f
Chandler <lcchrty@gmail.com>
2023-07-29 14:00:17 -0700

    hamburger nav

--- client/components/NavBar/HamburgerNav.jsx
+++ client/components/NavBar/HamburgerNav.jsx
@@ -1,35 +1,35 @@
-import React, { useState } from 'react';
-import { IoMenuOutline, IoClose } from 'react-icons/io5';
-import { GiLightBackpack } from 'react-icons/gi'
-import NavLinks from './NavLinks';
+import React, { useState } from "react";
+import { IoMenuOutline, IoClose } from "react-icons/io5";
+import { GiLightBackpack } from "react-icons/gi";
+import NavLinks from "./NavLinks";
 
 const HamburgerNav = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const hamburgerIcon = (
-        <IoMenuOutline
-            className="MenuIcon"
-            size="40px"
-            color="black"
-            onClick={() => setOpen(!open)}
-        />
-    );
+  const hamburgerIcon = (
+    <IoMenuOutline
+      className="MenuIcon"
+      size="40px"
+      color="white"
+      onClick={() => setOpen(!open)}
+    />
+  );
 
-    const closeIcon = (
-        <IoClose
-            className="MenuIcon"
-            size="40px"
-            color="black"
-            onClick={() => setOpen(!open)}
-        />
-    );
+  const closeIcon = (
+    <IoClose
+      className="MenuIcon"
+      size="40px"
+      color="white"
+      onClick={() => setOpen(!open)}
+    />
+  );
 
-    return (
-        <nav className="HamburgerNav">
-          <div className="flex flex-col absolute -top-2 left-0">
-            <div className="flex items-center gap-2">
-              <h1
-                className="
+  return (
+    <nav className="HamburgerNav">
+      <div className="flex flex-col absolute -top-2 left-0">
+        <div className="flex items-center gap-2">
+          <h1
+            className="
                   text-3xl
                   flex
                   gap-2
@@ -40,18 +40,18 @@ const HamburgerNav = () => {
                   rounded-full
                   pointer-events-none
               "
-              >
-                Adventure Connect
-                <GiLightBackpack className="text-blue-500" size={30} />
-              </h1>
-            </div>
-          </div>
-            {open ? closeIcon : hamburgerIcon}
-            <div className={open ? 'menu show' : 'menu hide'}>
-                <NavLinks/>
-            </div>
-        </nav>
-    );
+          >
+            Adventure Connect
+            <GiLightBackpack className="text-blue-500" size={30} />
+          </h1>
+        </div>
+      </div>
+      {open ? closeIcon : hamburgerIcon}
+      <div className={open ? "menu show" : "menu hide"}>
+        <NavLinks />
+      </div>
+    </nav>
+  );
 };
 
 export default HamburgerNav;
